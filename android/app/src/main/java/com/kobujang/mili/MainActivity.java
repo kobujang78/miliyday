6da9447ac68f3f5ba1d4b96c1a5348d02c619351
Master <kobujang78@gmail.com>
2026-03-13 16:22:24 +0900

    feat: add Android platform

---
+++ android/app/src/main/java/com/kobujang/mili/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kobujang.mili;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kobujang.mili',
+  appName: 'MiliConnect',
+  webDir: 'out'
+};
+
+export default config;
